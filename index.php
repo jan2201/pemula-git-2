673b9835103340df7f27fec6fa43d20741c06b4b
jan2201 <aojanmujiono11@gmail.com>
2024-06-15 22:07:37 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan git 2</title>
+</head>
+<body>
+    <h2>selmat datang</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, ad nesciunt provident temporibus eius illo ducimus magnam dicta voluptate. Corrupti ratione voluptatem unde commodi atque fugiat quo non eum iste.</p>
+</body>
+</html>
